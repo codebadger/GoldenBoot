42a30fce3b892a132368027b6f307a25c112a3d9
Brad Tutterow <btutterow@swankmp.com>
2016-10-19 13:58:16 -0500

    02 -- Make it an MVC app

--- Startup.cs
+++ Startup.cs
@@ -2,13 +2,21 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GoldenBoot
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
+
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello from ASP.NET Core!");
