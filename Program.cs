8bdd8553078080eb841d9d6501a0a93e18526113
Brad Tutterow <btutterow@swankmp.com>
2016-10-19 11:23:51 -0500

    00 -- Blank .NET Core App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoldenBoot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
