a84b5097df8240e62efbbffb2e293678fe3e8aff
Brad Tutterow <btutterow@swankmp.com>
2016-10-19 14:11:10 -0500

    04 -- EF Core

---
+++ api/AboutController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenBoot
+{
+    public class AboutController : Controller
+    {   
+        [Route("api/about")]
+        public JsonResult Index()
+        {
+            var OSDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            var OSArchitecture = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture;
+            var ProcessArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture;
+
+            return Json(new {
+                OSDescription,
+                OSArchitecture,
+                ProcessArchitecture
+            });
+        }
+    }
+}
---
+++ api/GoldenBootContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace GoldenBoot
+{
+    public partial class GoldenBootContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            var config = builder.Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("GoldenBoot"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Competition>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.IdNavigation)
+                    .WithOne(p => p.InverseIdNavigation)
+                    .HasForeignKey<Competition>(d => d.Id)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_Competition_Competition");
+            });
+
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.Property(e => e.Club)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Competition)
+                    .WithMany(p => p.Players)
+                    .HasForeignKey(d => d.CompetitionId)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_Player_Competition");
+            });
+        }
+
+        public virtual DbSet<Competition> Competition { get; set; }
+        public virtual DbSet<Player> Player { get; set; }
+    }
+}
