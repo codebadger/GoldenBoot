a9f64c25cedd10f2c0b10fe2e976f22aee250433
Brad Tutterow <btutterow@swankmp.com>
2016-12-21 16:45:59 -0600

    Started on mongoDB

---
+++ api/CompetitionMongoRepository.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+
+namespace GoldenBoot
+{
+    public class CompetitionMongoRepository : ICompetitionRepository
+    {
+        public Competition Get(string code)
+        {
+            return null;   
+        }
+    }
+
+}
--- api/Player.cs
+++ api/Player.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GoldenBoot
+﻿namespace GoldenBoot
 {
     public partial class Player
     {
