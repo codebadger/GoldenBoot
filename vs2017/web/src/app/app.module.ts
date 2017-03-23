f32747c42950ae4673d569e926ef3826fd6e0cfd
Brad Tutterow <btutterow@reliaslearning.com>
2017-03-23 15:29:43 -0500

    Routing and components for Angular app

--- vs2017/web/src/app/app.module.ts
+++ vs2017/web/src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CompetitionComponent } from './competition/competition.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  { path: '', component: CompetitionComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'competition', component: CompetitionComponent },
+  { path: 'competition/:id', component: CompetitionComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
