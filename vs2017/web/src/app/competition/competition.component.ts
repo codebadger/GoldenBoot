b25c1b4184817fd23e555b327511b0c80753f965
Brad Tutterow <btutterow@reliaslearning.com>
2017-03-29 15:08:02 -0500

    Writed up Competition and About controllers to services and to APIs

--- vs2017/web/src/app/competition/competition.component.ts
+++ vs2017/web/src/app/competition/competition.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
+
+import { CompetitionService } from './competition.service';
+import { Competition } from './competition';
 
 @Component({
   selector: 'app-competition',
   templateUrl: './competition.component.html',
-  styleUrls: ['./competition.component.css']
+  styleUrls: ['./competition.component.css'],
+  providers: [CompetitionService]
 })
 export class CompetitionComponent implements OnInit {
 
-  constructor() { }
+    error: string;
+    competition: Competition;
 
-  ngOnInit() {
-  }
+    constructor(private service: CompetitionService, private route: ActivatedRoute) {
+        this.competition = new Competition('one', 'copa', null);
+    }
+
+    ngOnInit() {
+
+        this.route.params
+            .map(x => x['id'])
+            .subscribe((id) => {
+
+                if (!id) { id = 'copa-2016'; };
+
+                this.service.getCompetition(id).subscribe(
+                    x => this.competition = x,
+                    error => this.error = <any>error
+                );
+
+        });
+
+    }
 
 }
